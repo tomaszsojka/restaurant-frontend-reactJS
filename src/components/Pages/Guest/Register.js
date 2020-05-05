242a0ed1d9282f15ade5460d4b0a4190e55a1bd2
tomaszsojka <tomek.s@windowslive.com>
2020-05-05 21:26:46 +0200

    Register form with empty validation

--- src/components/Pages/Guest/Register.js
+++ src/components/Pages/Guest/Register.js
@@ -3,15 +3,89 @@ import classes from "./AccessAccount.module.css";
 
 
 export class Register extends React.Component {
-
+//TODO if we have time password strength bar
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            email : "",
+            phoneNumber: "",
+            password: "",
+            errors: []
+        };
     }
 
-    submitRegister(e) {}
+    showValidationErr(elm, msg) {
+        this.setState((prevState) => ({
+                errors: [
+                    ...prevState.errors,
+                    {elm, msg}
+                    ]
+            })
+        );
+    }
+
+    clearValidationErr(elm) {
+        this.setState((prevState) => {
+            let newArr = [];
+            for(let err of prevState.errors) {
+                if(elm != err.elm) {
+                    newArr.push(err);
+                }
+            }
+            return {errors: newArr};
+        });
+    }
+
+    onEmailChange(e) {
+        this.setState({
+            email: e.target.value
+        });
+        this.clearValidationErr("email");
+
+    }
+    onPhoneNumberChange(e) {
+        this.setState({
+            phoneNumber: e.target.value
+        });
+        this.clearValidationErr("phoneNumber");
+    }
+    onPasswordChange(e) {
+        this.setState({
+            password: e.target.value
+        });
+        this.clearValidationErr("password");
+
+    }
+
+    submitRegister(e) {
+
+        if(this.state.phoneNumber == "") {
+            this.showValidationErr("phoneNumber", "Phone number cannot be empty");
+        }
+        if(this.state.email == "") {
+            this.showValidationErr("email", "Email address cannot be empty");
+        }
+        if(this.state.password == "") {
+            this.showValidationErr("password", "Password cannot be empty");
+        }
+    }
 
     render() {
+
+        let phoneNumberErr = null, emailErr = null, passwordErr = null;
+
+        for(let err of this.state.errors) {
+            if(err.elm == "phoneNumber") {
+                phoneNumberErr = err.msg;
+            }
+            if(err.elm == "email") {
+                emailErr = err.msg;
+            }
+            if(err.elm == "password") {
+                passwordErr = err.msg;
+            }
+        }
+
         return (
                 <div className={classes.innerContainer}>
                     <div className={classes.header}>
@@ -25,13 +99,23 @@ export class Register extends React.Component {
                                 type="tel"
                                 name="phone_number"
                                 className={classes.loginInput}
-                                pattern={"[0-9]{3}[ -]?[0-9]{2}[ -]?[0-9]{3}"}
-                                placeholder="Phone Number"/>
+                                /*pattern={"[0-9]{3}[ -]?[0-9]{2}[ -]?[0-9]{3}"}*/
+                                placeholder="Phone Number"
+                                onChange={this.onPhoneNumberChange.bind(this)}
+                            />
+                            <small className={classes.dangerError}>{ phoneNumberErr ? phoneNumberErr : "" }</small>
                         </div>
 
                         <div className={classes.inputGroup}>
                             <label htmlFor="email">Email</label>
-                            <input type="text" name="email" className={classes.loginInput} placeholder="Email"/>
+                            <input
+                                type="text"
+                                name="email"
+                                className={classes.loginInput}
+                                placeholder="Email"
+                                onChange={this.onEmailChange.bind(this)}
+                            />
+                            <small className={classes.dangerError}>{ emailErr ? emailErr : "" }</small>
                         </div>
 
                         <div className={classes.inputGroup}>
@@ -40,7 +124,10 @@ export class Register extends React.Component {
                                 type="password"
                                 name="password"
                                 className={classes.loginInput}
-                                placeholder="Password"/>
+                                placeholder="Password"
+                                onChange={this.onPasswordChange.bind(this)}
+                            />
+                            <small className={classes.dangerError}>{ passwordErr ? passwordErr : "" }</small>
                         </div>
                         <button
                             type="button"
