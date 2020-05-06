c9642b0ae9c74588a7fb4600871147cc632ec12f
tomaszsojka <tomek.s@windowslive.com>
2020-05-06 04:49:12 +0200

    Button linking to login page added on toolbar. Styling have to be revised

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "./Toolbar.css";
+import {NavLink} from "react-router-dom";
 import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
 const Toolbar = props => (
   <header className="toolbar">
         <nav className="toolbar_navigation">
-
-  <div className="toolbar_logo"><a href="/">LOGO</a></div>
+            <div className="toolbar_logo"><a href="/">LOGO</a></div>
             <div className="spacer" ></div>
             <div className={"toolbar_navigation_items"}>
-      <ul>
-          <li><a href="/menu">Menu</a></li>
-          <li><a href="/gallery">Gallery</a></li>
-          <li><a href="/">Home</a></li>
-          <li><a href="/contact">Contact</a></li>
-      </ul>
-  </div>
-            <div>
+                <ul>
+                    <li><a href="/menu">Menu</a></li>
+                    <li><a href="/gallery">Gallery</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </div>
+            <div className={"toolbar_navigation"}>
+                <NavLink className="acc_btn" activeClassName="active_acc_btn" to="/access_account">
+                    <button className="acc_btn" type="button">
+                        Account
+                    </button>
+                </NavLink>
                 <DrawerToggleButton click={props.drawerClickHandler}/>
             </div>
         </nav>
